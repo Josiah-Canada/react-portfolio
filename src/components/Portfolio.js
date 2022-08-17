4aa24f11cf5db4646590ce09a12ec18c67ad07a9
Josiah-Canada <josiahcanada@yahoo.com>
2022-08-17 12:53:23 -0400

    1.0

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,28 +1,33 @@
 import React from "react";
 import Project from './Project'
-import Budget from '../images/budget.png'
+import Budget from '../images/Budget.png'
+import Run from '../images/Run-buddy.png'
+import Team from '../images/Teamwork.jpeg'
+import Work from '../images/Work.png'
+import Gen from '../images/Gen.png'
+import Weather from '../images/Weather.png'
 
 
 function Portfolio() {
     return (
-      <section>
+      <section id = "hide">
 
-       <Project  title = "Budget tracker" image= {Budget} github = "https://github.com/Josiah-Canada/Budget-tracker" deployment= "https://evening-shore-05535.herokuapp.com/">
-
-       </Project>
-       <Project title = "Run buddy" image = {Budget} github = "https://github.com/Josiah-Canada/run-buddy" deployment= "https://josiah-canada.github.io/run-buddy/">
+       <Project title = "Budget tracker" image= {Budget} github = "https://github.com/Josiah-Canada/Budget-tracker" deployment= "https://evening-shore-05535.herokuapp.com/">
 
        </Project>
-       <Project title = "Team Profile Generator" image = {Budget} github = "https://github.com/Josiah-Canada/team-profile-generator" deployment= "https://josiah-canada.github.io/team-profile-generator/">
+       <Project title = "Run buddy" image = {Run} github = "https://github.com/Josiah-Canada/run-buddy" deployment= "https://josiah-canada.github.io/run-buddy/">
 
        </Project>
-       <Project title = "Work day scheduler" image = {Budget} github = "https://github.com/Josiah-Canada/calender" deployment = "https://josiah-canada.github.io/calender/">
+       <Project title = "Team Profile Generator" image = {Team} github = "https://github.com/Josiah-Canada/team-profile-generator" deployment= "https://josiah-canada.github.io/team-profile-generator/">
 
        </Project>
-       <Project title = "Password generator" image = {Budget} github = "https://github.com/Josiah-Canada/password-generator" deployment = "https://josiah-canada.github.io/password-generator/">
+       <Project title = "Work day scheduler" image = {Work} github = "https://github.com/Josiah-Canada/calender" deployment = "https://josiah-canada.github.io/calender/">
 
        </Project>
-       <Project title = "Weather dashboard" image = {Budget} github = "https://github.com/Josiah-Canada/weather-dashboard" deployment = "https://josiah-canada.github.io/weather-dashboard/">
+       <Project title = "Password generator" image = {Gen} github = "https://github.com/Josiah-Canada/password-generator" deployment = "https://josiah-canada.github.io/password-generator/">
+
+       </Project>
+       <Project title = "Weather dashboard" image = {Weather} github = "https://github.com/Josiah-Canada/weather-dashboard" deployment = "https://josiah-canada.github.io/weather-dashboard/">
         
        </Project>
       </section>
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,12 +2,14 @@ import React from "react";
 
 function Project({title, image, github, deployment, linkdn}) {
     return (
-      <section >
+      <section className="pic">
        <h1 className = "project-title">{title}</h1> 
+       <section id="more">
        <img src = {image} alt = {title} width = {400}></img>
-       <a href = {github}>github</a>
-       {deployment ? <a href = {deployment}>deployment</a> : <a>{" "}</a>}
-       {linkdn ? <a href = {linkdn}>linkdn</a> : <a>{" "}</a>}
+       </section>
+       <a className="link" href = {github}>github</a>
+       {deployment ? <a className="link" href = {deployment}>deployment</a> : <a>{" "}</a>}
+       {linkdn ? <a className="link" href = {linkdn}>linkdn</a> : <a>{" "}</a>}
       </section>
     );
   }
