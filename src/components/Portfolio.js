bc02573185af5ffe7679de50703cfd490b7f4fb0
Josiah-Canada <josiahcanada@yahoo.com>
2022-12-29 18:22:54 -0500

    portfolio component fixes 1/2

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,34 +4,30 @@ import Budget from '../images/Budget.png'
 import Run from '../images/Run-buddy.png'
 import Team from '../images/Teamwork.jpeg'
 import Work from '../images/Work.png'
-import Gen from '../images/Gen.png'
 import Lingua from '../images/lingua.png'
 import Open from '../images/Opensignal.png'
 
 
 function Portfolio() {
     return (
-      <section>
+      <section id="projects">
 
-       <Project title = "Budget tracker" image= {Budget} github = "https://github.com/Josiah-Canada/Budget-tracker" deployment= "https://evening-shore-05535.herokuapp.com/">
-
-       </Project>
-       <Project title = "Run buddy" image = {Run} github = "https://github.com/Josiah-Canada/run-buddy" deployment= "https://josiah-canada.github.io/run-buddy/">
+       <Project  title = "Budget tracker" image= {Budget} github = "https://github.com/Josiah-Canada/Budget-tracker" deployment= "https://evening-shore-05535.herokuapp.com/">
 
        </Project>
-       <Project title = "Team Profile Generator" image = {Team} github = "https://github.com/Josiah-Canada/team-profile-generator" deployment= "https://josiah-canada.github.io/team-profile-generator/">
+       <Project  title = "Run buddy" image = {Run} github = "https://github.com/Josiah-Canada/run-buddy" deployment= "https://josiah-canada.github.io/run-buddy/">
 
        </Project>
-       <Project title = "Work day scheduler" image = {Work} github = "https://github.com/Josiah-Canada/calender" deployment = "https://josiah-canada.github.io/calender/">
+       <Project  title = "Team Profile Generator" image = {Team} github = "https://github.com/Josiah-Canada/team-profile-generator" deployment= "https://josiah-canada.github.io/team-profile-generator/">
 
        </Project>
-       <Project title = "Password generator" image = {Gen} github = "https://github.com/Josiah-Canada/password-generator" deployment = "https://josiah-canada.github.io/password-generator/">
+       <Project  title = "Work day scheduler" image = {Work} github = "https://github.com/Josiah-Canada/calender" deployment = "https://josiah-canada.github.io/calender/">
 
        </Project>
-       <Project title = "Lingua" image = {Lingua} github = "https://github.com/SamuelGa25/Project-2-Lingua" deployment = "https://josiah-canada.github.io/weather-dashboard/">
+       <Project  title = "Lingua" image = {Lingua} github = "https://github.com/SamuelGa25/Project-2-Lingua" deployment = "https://josiah-canada.github.io/weather-dashboard/">
         
        </Project>
-       <Project title = "Opensignal" image = {Open} github = "https://github.com/alandidiego/finalproject" deployment= "https://git.heroku.com/opensignal.git" >
+       <Project  title = "Opensignal" image = {Open} github = "https://github.com/alandidiego/finalproject" deployment= "https://git.heroku.com/opensignal.git" >
 
        </Project>
       </section>
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,7 @@ function Project({title, image, github, deployment, linkdin}) {
       <section className="pic">
        <h1 className = "project-title">{title}</h1> 
        <section id="more">
-       <img src = {image} alt = {title} width = {400}></img>
+       <img src = {image} alt = {title} width = {275}></img>
        </section>
        <a className="link" href = {github}>github</a>
        {deployment ? <a className="link" href = {deployment}>deployment</a> : <a>{" "}</a>}
