70c6fc86a01f16ad470ab12040a64ad42b17e19f
Josiah-Canada <josiahcanada@yahoo.com>
2023-01-10 15:57:30 -0500

    contact form fixes 1/3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,48 +1,31 @@
-import React, { useState,useRef } from 'react';
+import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
-import e from 'cors';
 
-function Contact() {
-   const form = useRef()
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    const { name, email, message } = formState;
+export const ContactUs = () => {
+  const form = useRef();
 
-    function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
-      }
-      
-      console.log(formState);
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-      const sendEmail = (e) => {
-        e.preventDefault();
-    
-        emailjs.sendForm('service_f6xbg0j', 'template_6f4tlub', form.current, 'JfzOdPoD5pB2z0jvb')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-      };
-    return (
-        <section>
-          <h1>Contact me</h1>
-          <form id="contact" onSubmit={sendEmail} >
-            <div>
-              <label htmlFor="name">Name:</label>
-              <input type="text" defaultValue={name} onChange={handleChange} name="name" />
-            </div>
-            <div>
-              <label htmlFor="email">Email address:</label>
-              <input type="email" defaultValue={email} name="email" onChange={handleChange} />
-            </div>
-            <div>
-              <label htmlFor="message">Message:</label>
-              <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
-            </div>
-            <button type="submit">Submit</button>
-          </form>
-        </section>
-        );
-}
+    emailjs.sendForm('service_00lepju', 'template_if295fd', form.current, 'JfzOdPoD5pB2z0jvb')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
-export default Contact;
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+
+export default ContactUs;
