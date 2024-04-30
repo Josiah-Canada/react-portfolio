6a127be024a3cd788c34ed74213581cd434a3de2
Josiah-Canada <josiahcanada@yahoo.com>
2024-04-30 18:43:04 -0400

    built

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,8 @@ function About() {
       <article>
       <div id= "card-a">
       <img src={Avatar} alt="cover" width = {200} />
-        <h1 id="about">Hi, I'm Josiah Canada! I am recently new to programming, however I do find it fascinating. I love the feeling of working on a project for days and learning the new technology as I work. The success that comes from finally completing a project that took hours to complete is undeniable, and it makes me wonder just how much can be done with the technologies we have available today. So if you're interested in seeing some of my work take a look around, you might like what you see.</h1>
+        <h1 id="about">Hi, I'm Josiah Canada! I am recently new to programming,and just finished a coding boot camp at the university of miami. I got into it to start a career change and I never regretted it. Since the boot camp I've been grinding my skills and I've learned so much about programming, and I'm ready to ut my skills to the test.</h1>
+        <p>Check out some of my projects and shoot me a message in the contact section!</p>
       </div>
       </article>
     );
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,36 +1,56 @@
 import React from "react";
-import Project from './Project'
-import Budget from '../images/Budget.png'
-import Run from '../images/Run-buddy.png'
-import Team from '../images/Teamwork.jpeg'
-import Work from '../images/Work.png'
-import Lingua from '../images/lingua.png'
-import Open from '../images/Opensignal.png'
+import Project from "./Project";
+import Budget from "../images/Budget.png";
+import Run from "../images/Run-buddy.png";
+import Work from "../images/Work.png";
+import Glam from "../images/glam.png";
+import Todo from "../images/todo.png";
+import Musicfind from "../images/music-find.png"
+
 
 
 function Portfolio() {
-    return (
-      <section id="projects">
-       <Project title = "Budget tracker" image= {Budget} github = "https://github.com/Josiah-Canada/Budget-tracker" deployment= "https://evening-shore-05535.herokuapp.com/">
-        
-       </Project>
-       <Project  title = "Run buddy" image = {Run} github = "https://github.com/Josiah-Canada/run-buddy" deployment= "https://josiah-canada.github.io/run-buddy/">
+  return (
+    <section id="projects">
+      <Project
+        title="Budget tracker"
+        image={Budget}
+        github="https://github.com/Josiah-Canada/Budget-tracker"
+        deployment="https://evening-shore-05535.herokuapp.com/"
+      ></Project>
+      <Project
+        title="Run buddy"
+        image={Run}
+        github="https://github.com/Josiah-Canada/run-buddy"
+        deployment="https://josiah-canada.github.io/run-buddy/"
+      ></Project>
+      
+      <Project
+        title="Work day scheduler"
+        image={Work}
+        github="https://github.com/Josiah-Canada/calender"
+        deployment="https://josiah-canada.github.io/calender/"
+      ></Project>
+      <Project
+        title="Music find"
+        image={Musicfind}
+        github="https://github.com/justjulio95/music-find"
+        deployment="https://justjulio95.github.io/music-find/"
+      ></Project>
+      <Project
+        title="glam on glitz"
+        image={Glam}
+        github="https://github.com/Josiah-Canada/glam-on-glitzzz"
+        deployment="https://josiah-canada.github.io/glam-on-glitzzz/"
+      ></Project>
+      <Project
+        title="todo"
+        image={Todo}
+        github="https://github.com/Josiah-Canada/todo---frontend"
+        deployment="https://main--calm-empanada-bac4c8.netlify.app/"
+      ></Project>
+    </section>
+  );
+}
 
-       </Project>
-       <Project  title = "Team Profile Generator" image = {Team} github = "https://github.com/Josiah-Canada/team-profile-generator" deployment= "https://josiah-canada.github.io/team-profile-generator/">
-
-       </Project>
-       <Project  title = "Work day scheduler" image = {Work} github = "https://github.com/Josiah-Canada/calender" deployment = "https://josiah-canada.github.io/calender/">
-
-       </Project>
-       <Project  title = "Lingua" image = {Lingua} github = "https://github.com/SamuelGa25/Project-2-Lingua" deployment = "https://josiah-canada.github.io/weather-dashboard/">
-        
-       </Project>
-       <Project  title = "Opensignal" image = {Open} github = "https://github.com/alandidiego/finalproject" deployment= "https://git.heroku.com/opensignal.git" >
-
-       </Project>
-      </section>
-    );
-  }
-  
-  export default Portfolio;
+export default Portfolio;
